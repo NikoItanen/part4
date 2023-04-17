060a221ba44d4ff90db52540fa7f40b2b340a52a
NikoItanen <itanenniko@gmail.com>
2023-04-17 12:55:03 +0300

    4-10 Completed!

--- models/user.js
+++ models/user.js
@@ -1,25 +1,35 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minlength: 3
     },
-    passwordHash: String,
-    name: String
-})
+    name: String,
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
+});
 
 userSchema.plugin(uniqueValidator)
 
 
 userSchema.set('toJSON', {
-    transform: (document, returedObject) => {
-        returedObject.id = returedObject._id.toString()
-        delete returedObject._id
-        delete returedObject.__v
-        delete returedObject.passwordHash
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
     }
 })
 
